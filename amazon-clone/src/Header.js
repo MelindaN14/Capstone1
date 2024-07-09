650142c054a212d5750b690666e269f610ca1b1b
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-07-08 20:35:21 -0400

    css

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Header.css";
 function Header() {
-  return <div className="Header"></div>;
+  return (
+    <div className="Header">
+      <img src="" />
+    </div>
+  );
 }
 
 export default Header;
